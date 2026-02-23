2f6806fe46d1109fd0d6bf5c5563de86a269b82b
saichowdary8254 <saichowdary8259@gmail.com>
2026-02-23 18:58:57 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "rg_name" {
+    type = string
+    description = "Resource Group Name"  
+}
+
+variable "rg_location" {
+    type = string  
+}
+
+variable "acr_name" {
+    type = string  
+}
+
+variable "aks_name" {
+    type = string  
+}
+
+variable "dns_prefix" {
+    type = string  
+}
