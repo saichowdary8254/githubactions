64c8cf2e9d40f8cf5a5dd4ea1da9ea5562b8bdf4
saichowdary8254 <saichowdary8259@gmail.com>
2026-02-23 18:58:25 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+ We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.58.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "bad976a3-efdf-4d47-b37c-2eaf2b10b6ec"
+}
