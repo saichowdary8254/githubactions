b2939efe84fcbeeccea6618fd31a53db2556b14b
saichowdary8254 <saichowdary8259@gmail.com>
2026-02-24 13:48:30 +0530

    provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,6 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
 terraform {
-  required_version = ">= 1.5.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -10,14 +8,18 @@ terraform {
     }
   }
 
-  # --- Remote backend: Azure Storage ---
-  # Option A (Inline config - okay for PoC; avoid committing secrets):
   backend "azurerm" {
-    resource_group_name  = "github-actions"          # change to your RG
-    storage_account_name = "githubremotebackend"   # must be globally unique
-    container_name       = "test-container"             # blob container
-    key                  = "terraforms.tfstate"
-    # (Optional) use a state lock table for concurrency protection if using Cosmos DB-based locks or rely on blob leases.
-    # (Optional) use 'subscription_id' here if your state RG is in a different subscription
+    resource_group_name  = "githubremotebackend"  
+    storage_account_name = "githubremotebackend"                      
+    container_name       = "test-container"                       
+    key                  = "terraform.tfstate"       
   }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+
+  subscription_id = "bad976a3-efdf-4d47-b37c-2eaf2b10b6ec"
 }
