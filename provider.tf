13383150ca4d8a9f7a99ad8efb25527677f31ff7
saichowdary8254 <saichowdary8259@gmail.com>
2026-02-24 11:19:47 +0530

    provider.tf

--- provider.tf
+++ provider.tf
@@ -1,16 +1,23 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.58.0"
+      version = "=4.61.0"
     }
   }
-}
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-  subscription_id = "bad976a3-efdf-4d47-b37c-2eaf2b10b6ec"
+  # --- Remote backend: Azure Storage ---
+  # Option A (Inline config - okay for PoC; avoid committing secrets):
+  backend "azurerm" {
+    resource_group_name  = "github-actions"          # change to your RG
+    storage_account_name = "githubremotebackend"   # must be globally unique
+    container_name       = "test-container"             # blob container
+    key                  = "terraforms.tfstate"
+    # (Optional) use a state lock table for concurrency protection if using Cosmos DB-based locks or rely on blob leases.
+    # (Optional) use 'subscription_id' here if your state RG is in a different subscription
+  }
 }
